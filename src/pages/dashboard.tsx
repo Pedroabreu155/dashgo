85b068e6a0f606757176d70620454c2ae5b4f98f
Pedro <pedroliveirabreu@gmail.com>
2021-08-01 21:38:56 -0300

    created header component

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Header } from '../components/Header';
+
+export default function Dashboard() {
+  return (
+    <>
+      <Header />
+      <h1>Dashboard</h1>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,33 @@
-import Head from 'next/head';
+import { Flex, Button, Stack, Text } from '@chakra-ui/react';
 
-export default function Home() {
-  return <h1>Dashgo</h1>;
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
+  return (
+    <Flex w="100vw" h="100vh" flexDir="column" align="center" justify="center">
+      <Text fontSize="6xl" fontWeight="bold" letterSpacing="tight" mb="8">
+        dashgo
+        <Text as="span" ml="1" color="pink.500">
+          .
+        </Text>
+      </Text>
+      <Flex
+        as="form"
+        w="100%"
+        flexDir="column"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
